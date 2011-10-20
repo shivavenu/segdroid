ddc872072ccab0edc5258f0f1ffd159948e579b8
beanxlj@gmail.com <rogerxue@dhcp-172-26-117-83.nyc.corp.google.com>
2011-10-20 16:09:40 -0400

    1

--- android/segdroid/src/com/rogerxue/android/selfbalance/MainActivity.java
+++ android/segdroid/src/com/rogerxue/android/selfbalance/MainActivity.java
@@ -3,7 +3,6 @@ package com.rogerxue.android.selfbalance;
 import java.util.ArrayList;
 
 import android.app.TabActivity;
-import android.content.Context;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.os.PowerManager;
@@ -17,31 +16,33 @@ import android.widget.TextView;
 
 import com.rogerxue.android.selfbalance.model.DynamicPlotable2D;
 
-public class MainActivity extends TabActivity {
-  private MainView mainView;
+public class MainActivity extends TabActivity implements InclineCalculator.Observer{
+  private static final int MAX = 470;
   private PowerManager mPowerManager;
   private WakeLock mWakeLock;
   private InclineCalculator inclineCalculator;
   private TabHost mTabHost;
+  private ECGView ecgView;
+  private LinearLayout mLinearLayout;
+  private TextView tv;
+  DynamicPlotable2D data;
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
     mTabHost = getTabHost();
-
     
     mPowerManager = (PowerManager) getSystemService(POWER_SERVICE);
     mWakeLock = mPowerManager.newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK, getClass()
       .getName());
-    mainView = new MainView(this);
-    setupTab(mainView.mLinearLayout, "visualize");
+    setupMainView();
+    setupTab(mLinearLayout, "visualize");
     setupTab(new TextView(this), "something");
     mTabHost.setCurrentTab(0);
-    
-    
+
     inclineCalculator = new InclineCalculator(this);
-    inclineCalculator.addObserver(mainView);
+    inclineCalculator.addObserver(this);
   }
 
   private void setupTab(final View view, final String tag) {
@@ -67,7 +68,6 @@ public class MainActivity extends TabActivity {
     this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
     inclineCalculator.start();
-    mainView.prepare();
   }
 
   @Override
@@ -79,63 +79,50 @@ public class MainActivity extends TabActivity {
     mWakeLock.release();
   }
 
-  class MainView extends View implements InclineCalculator.Observer {
-    private static final int MAX = 470;
-    private ECGView ecgView;
-    private LinearLayout mLinearLayout;
-    private TextView tv;
-    DynamicPlotable2D data;
+  private void setupMainView() {
+    ArrayList<String> dataNames = new ArrayList<String>();
+    dataNames.add("inclineX");
+    dataNames.add("inclineY");
+    dataNames.add("inlcineZ");
+    dataNames.add("inclineDerX");
+    dataNames.add("inclineDerY");
+    dataNames.add("inclineDerZ");
+    data = new DynamicPlotable2D(dataNames, MAX);
+    tv = new TextView(this);
+    ecgView = new ECGView(this, data, 10, 1, MAX, 400);
+    mLinearLayout = new LinearLayout(this);
+    mLinearLayout.setOrientation(LinearLayout.VERTICAL);
+    mLinearLayout.addView(tv);
+    mLinearLayout.addView(ecgView);
+  }
 
-    public MainView(Context context) {
-      super(context);
-      ArrayList<String> dataNames = new ArrayList<String>();
-      dataNames.add("inclineX");
-      dataNames.add("inclineY");
-      dataNames.add("inlcineZ");
-      dataNames.add("inclineDerX");
-      dataNames.add("inclineDerY");
-      dataNames.add("inclineDerZ");
-      data = new DynamicPlotable2D(dataNames, MAX);
-      tv = new TextView(context);
-      ecgView = new ECGView(context, data, 10, 1, MAX, 400);
-      mLinearLayout = new LinearLayout(context);
-      mLinearLayout.setOrientation(LinearLayout.VERTICAL);
-      mLinearLayout.addView(tv);
-      mLinearLayout.addView(ecgView);
-    }
+  private void setContent(float xAcc, float yAcc, float zAcc, float xGyro, float yGyro,
+                          float zGyro) {
+    tv.setText(
+      "X acc:" + xAcc + 
+      "\nY acc:" + yAcc +
+      "\nZ acc:" + zAcc +
+      "\nX gyr:" + xGyro + 
+      "\nY gyr:" + yGyro + 
+      "\nZ gyr:" + zGyro);
+  }
 
-    public void prepare() {
-//      set
-    }
-
-    @Override
-    public void onData() {
-      data.add(new float[] {
-                            inclineCalculator.getIncline()[0],
-                            inclineCalculator.getIncline()[1],
-                            inclineCalculator.getIncline()[2],
-                            inclineCalculator.getInclineDerivative()[0],
-                            inclineCalculator.getInclineDerivative()[1],
-                            inclineCalculator.getInclineDerivative()[2]});
-      setContent(
+  @Override
+  public void onData() {
+    data.add(new float[] {
+        inclineCalculator.getIncline()[0],
+        inclineCalculator.getIncline()[1],
+        inclineCalculator.getIncline()[2],
+        inclineCalculator.getInclineDerivative()[0],
+        inclineCalculator.getInclineDerivative()[1],
+        inclineCalculator.getInclineDerivative()[2]});
+    setContent(
         inclineCalculator.getAccIncline()[0],
         inclineCalculator.getAccIncline()[1],
         inclineCalculator.getAccIncline()[2],
         inclineCalculator.getIncline()[0],
         inclineCalculator.getIncline()[1],
         inclineCalculator.getIncline()[2]);
-      ecgView.invalidate();
-    }
-
-    private void setContent(float xAcc, float yAcc, float zAcc, float xGyro, float yGyro,
-                            float zGyro) {
-      tv.setText(
-        "X acc:" + xAcc + 
-        "\nY acc:" + yAcc +
-        "\nZ acc:" + zAcc +
-        "\nX gyr:" + xGyro + 
-        "\nY gyr:" + yGyro + 
-        "\nZ gyr:" + zGyro);
-    }
+    ecgView.invalidate();
   }
 }
